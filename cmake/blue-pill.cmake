65321342229c7d248bac27aa8ef786164344772a
Mateusz Bugaj <mateuszbugaj5@gmail.com>
2023-12-30 22:10:24 +0100

    Added UART communication

--- cmake/blue-pill.cmake
+++ cmake/blue-pill.cmake
@@ -1,6 +1,6 @@
 # STM32F1 Blue-Pill flags
 add_definitions(-DSTM32F1)
-set(STM32F1_FLAGS "-Os -g -mcpu=cortex-m3 -mthumb -msoft-float -MD -u _printf_float -specs=nosys.specs")
+set(STM32F1_FLAGS "-g -Os -mcpu=cortex-m3 -mthumb -msoft-float -MD -u _printf_float -specs=nosys.specs") # -Os (opt for size, remove for gdb debugging)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${STM32F1_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${STM32F1_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --static -nostartfiles -MD -T '${CMAKE_SOURCE_DIR}/cmake/stm32f103x8.ld'")
--- src/Lidar.cpp
+++ src/Lidar.cpp
@@ -1,31 +1,87 @@
-#include "FreeRTOS.h"
-#include "task.h"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName);
+static QueueHandle_t uart_txq;		// TX queue for UART
+static uint16_t timer_counter = 0;
 
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-	(void)xTask;
-	(void)pcTaskName;
-	for(;;);
+static void uart_setup(void) {
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_USART1);
+
+	// UART TX on PA9 (GPIO_USART1_TX)
+	gpio_set_mode(GPIOA,
+		GPIO_MODE_OUTPUT_50_MHZ,
+		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+		GPIO_USART1_TX);
+
+	usart_set_baudrate(USART1,38400);
+	usart_set_databits(USART1,8);
+	usart_set_stopbits(USART1,USART_STOPBITS_1);
+	usart_set_mode(USART1,USART_MODE_TX);
+	usart_set_parity(USART1,USART_PARITY_NONE);
+	usart_set_flow_control(USART1,USART_FLOWCONTROL_NONE);
+	usart_enable(USART1);
+
+	// Create a queue for data to transmit from UART
+	uart_txq = xQueueCreate(256,sizeof(char));
 }
 
-static void
-task1(void *args __attribute((unused))) {
+static void uart_task(void *args __attribute__((unused))) {
+	char ch;
 
 	for (;;) {
-		gpio_toggle(GPIOC,GPIO13);
-		vTaskDelay(pdMS_TO_TICKS(100));
+		// Receive char to be TX
+		if ( xQueueReceive(uart_txq,&ch,500) == pdPASS ) {
+			while ( !usart_get_flag(USART1,USART_SR_TXE) )
+				taskYIELD();	// Yield until ready
+			usart_send(USART1,ch);
+		}
 	}
 }
 
-int
-main(void) {
+static void uart_puts(const char *s) {
+	for ( ; *s; ++s ) {
+		xQueueSend(uart_txq,s,portMAX_DELAY); 
+	}
+}
 
-	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]); // For "blue pill"
+static void uart_putn(uint16_t n) {
+	char buff[10] {0};
+	uint8_t digitNumber = 0;
 
+	while(n > 0){
+		char c = n % 10 + '0';
+		buff[digitNumber++] = c;
+
+		n /= 10;
+	}
+
+	while(digitNumber > 0){
+		xQueueSend(uart_txq,&buff[digitNumber-1],portMAX_DELAY);
+		digitNumber--;
+	}
+}
+
+static void demo_task(void *args __attribute__((unused))) {
+	for (;;) {
+		uart_putn(timer_counter);
+		uart_puts("\n\r");
+		timer_counter++;
+
+		gpio_toggle(GPIOC,GPIO13);
+		vTaskDelay(pdMS_TO_TICKS(10));
+	}
+}
+
+int main(void) {
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// CPU clock is 72 MHz
+
+	// GPIO PC13:
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(
 		GPIOC,
@@ -33,11 +89,14 @@ main(void) {
 		GPIO_CNF_OUTPUT_PUSHPULL,
 		GPIO13);
 
-	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
-	vTaskStartScheduler();
+	uart_setup();
 
+	xTaskCreate(uart_task,"UART",100,NULL,configMAX_PRIORITIES-1,NULL);
+	xTaskCreate(demo_task,"DEMO",100,NULL,configMAX_PRIORITIES-1,NULL);
+
+	vTaskStartScheduler();
 	for (;;);
 	return 0;
 }
 
-// End
+/* End */
